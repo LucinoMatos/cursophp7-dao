c96b1a6d01a9548c1f6225600e2569750f4e92e7
Lucino Matos <lucino.sousa.ls@gmail.com>
2018-07-20 21:54:33 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -6,11 +6,32 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
-$LucinoMatos = new Usuario();
 
-$LucinoMatos->loadbyId(9);
+//carrega um usuário
+//$LucinoMatos = new Usuario();
 
-echo $LucinoMatos;
+//$LucinoMatos->loadbyId(9);
+//echo $LucinoMatos;
+
+
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uama lista de usuários buscando pelo login;
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//Carrega um usuário usando login e a senha;
+
+$usuario = new Usuario();
+$usuario->login("root", "123456");
+
+echo $usuario;
 
 
 ?>
